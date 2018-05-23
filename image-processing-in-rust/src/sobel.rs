64defaae8a2bd24eda5e238f11970018e10ad682
heath00 <HeathReineke2018@u.northwestern.edu>
2018-05-22 21:34:25 -0500

    sobel working pretty much

--- image-processing-in-rust/src/sobel.rs
+++ image-processing-in-rust/src/sobel.rs
@@ -1,27 +1,34 @@
 extern crate image;
 use std::fs::File;
-use image::GenericImage;
+use image::{GenericImage, Pixel};
 
 pub fn sobel_detector_test() {
     let img = image::open("test.jpg").unwrap();
 
-    println!("dimensions: {:?}", img.dimensions());
-    println!("color : {:?}", img.grayscale().color());
-
     let mut gray_im = img.grayscale();
 
     let filt_x: &[f32] = &[-1f32,0f32,1f32,-2f32,0f32,2f32,-1f32,0f32,1f32];
-    let filt: &[f32] = &[-1f32,-2f32, -1f32, 0f32,0f32, 0f32, 1f32, 2f32,1f32];
-    let gray_x = gray_im.filter3x3(filt_x);
+    let filt_y: &[f32] = &[-1f32,-2f32, -1f32, 0f32,0f32, 0f32, 1f32, 2f32,1f32];
+    let mut gray_x = gray_im.filter3x3(filt_x);
     let gray_y = gray_im.filter3x3(filt_y);
 
     let mut sobeled_im = image::ImageBuffer::new(512, 512);
 
     for i in 0..512 {
         for j in 0..512 {
-            sobeled_im.get_pixel_mut(i, j) = (gray_x.get_pixel(i, j).pow(2));
+            let mut px1_sq = gray_x.get_pixel(i, j).channels()[0] as f32;
+            let mut px2_sq = gray_y.get_pixel(i, j).channels()[0] as f32;
+
+            px1_sq = px1_sq.powf(2.);
+            px2_sq = px2_sq.powf(2.);
+
+            let new_px = gray_x.get_pixel(i, j).map(|v| (px1_sq + px2_sq).sqrt().ceil() as u8);
+            sobeled_im.put_pixel(i, j, new_px);
         }
     }
 
-    gray_im.save("sobel.jpg");
+    gray_x.save("gray_x.jpg");
+    gray_y.save("gray_y.jpg");
+    sobeled_im.save("sobel.jpg");
+    
 }
