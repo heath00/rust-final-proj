1b17e63671f134e126209951985ad2840a42535d
heath00 <HeathReineke2018@u.northwestern.edu>
2018-05-31 19:36:46 -0500

    eigenfaces not turning out right..

--- image-processing-in-rust/src/eigenfaces.rs
+++ image-processing-in-rust/src/eigenfaces.rs
@@ -1,9 +1,11 @@
 extern crate image;
-use image::{GenericImage, Pixel};
-use std::fs::read_dir;
+use image::{GenericImage, Pixel, Luma};
+use std::fs::{read_dir, File};
+use std::path::Path;
 
 extern crate nalgebra as na;
 use self::na::{Dynamic, MatrixArray, MatrixVec, DMatrix, DVector,};
+
 
 pub fn handler() {
     let mut my_mat = create_column_vecs_na();
@@ -29,14 +31,17 @@ pub fn handler() {
         mat_holder.append(&mut Vec::from(this_vec.as_slice()));
     }
 
-    let mat_holder_as_slice: &[f32] = &mat_holder;
+    let mat_holder_as_slice: &[f64] = &mat_holder;
     let mut eigenvector_u = DMatrix::from_row_slice(my_mat.nrows(), my_mat.ncols(), mat_holder_as_slice);
 
     normalize_u(&mut eigenvector_u);
+    create_buffer(&eigenvector_u);
 
 }
 
-fn normalize_u(u: &mut DMatrix<f32>) {
+
+
+fn normalize_u(u: &mut DMatrix<f64>) {
 
     // keeps track of mins and maxs while we iterate through every value to get sum_of_sq
     let mut mins = Vec::with_capacity(u.ncols());
@@ -46,7 +51,7 @@ fn normalize_u(u: &mut DMatrix<f32>) {
         let mut this_min = u[(0, i)];
         let mut this_max = u[(0, i)];
 
-        let mut sum_of_sq = 0f32;
+        let mut sum_of_sq = 0f64;
 
         for j in 0..u.nrows() {
             sum_of_sq += u[(j, i)];
@@ -63,26 +68,63 @@ fn normalize_u(u: &mut DMatrix<f32>) {
 
     for i in 0..u.ncols() {
         for j in 0..u.nrows() {
-            u[(j,i)] = 255f32 * ((u[(j,i)] - mins[i]) / (maxs[i] - mins[i]));
+            u[(j,i)] = 255f64 * ((u[(j,i)] - mins[i]) / (maxs[i] - mins[i]));
             u[(j,i)] = u[(j,i)].floor();
         }
     }
-    println!("{}", u.column(0));
+
+}
+
+fn create_buffer(u: &DMatrix<f64>) {
+
+
+    let mut luma_holder = Vec::with_capacity(u.nrows());
+
+    for i in 0..u.nrows() {
+        luma_holder.push(Luma([u[(i, 0)] as u8]));
+        if i < 512 {
+            println!("{}", u[(i, 0)]);
+        }
+    }
+
+    for i in 0..luma_holder.len() {
+//        println!("{}", luma_holder[i].channels()[0]);
+    }
+
+
+    let mut buf = image::ImageBuffer::new(64, 64);
+
+
+    let mut count = 0;
+    for i in 0..64 {
+        for j in 0..64 {
+            buf.put_pixel(j, i, luma_holder[count]);
+            count += 1;
+        }
+    }
+
+
+
+    let mut fout = File::create("gen.png").unwrap();
+
+
+    image::ImageLuma8(buf).save("gen.png").unwrap();
+
 
 }
 
 
-fn get_covariant_mat_large(inp: DMatrix<f32>) -> DMatrix<f32> {
-    let transposed: DMatrix<f32> = inp.transpose();
+fn get_covariant_mat_large(inp: DMatrix<f64>) -> DMatrix<f64> {
+    let transposed: DMatrix<f64> = inp.transpose();
     inp * transposed
 }
 
-fn get_covariant_mat_small(inp: &DMatrix<f32>) -> DMatrix<f32> {
-    let transposed: DMatrix<f32> = inp.transpose();
+fn get_covariant_mat_small(inp: &DMatrix<f64>) -> DMatrix<f64> {
+    let transposed: DMatrix<f64> = inp.transpose();
     transposed * inp
 }
 
-fn img_to_1d(filename: &str, is_grayscale: bool) -> Vec<f32> {
+fn img_to_1d(filename: &str, is_grayscale: bool) -> Vec<f64> {
     let mut img = image::open(filename).unwrap();
     let (x,y) = img.dimensions();
 
@@ -96,14 +138,15 @@ fn img_to_1d(filename: &str, is_grayscale: bool) -> Vec<f32> {
 
     for i in 0..y {
         for j in 0..x {
-            vec_1d.push(img.get_pixel(i,j).channels()[0] as f32);
+            vec_1d.push(img.get_pixel(i,j).channels()[0] as f64);
         }
     }
 
+    println!("{}", vec_1d.len());
     vec_1d
 }
 
-fn create_column_vecs_na() -> DMatrix<f32> {
+fn create_column_vecs_na() -> DMatrix<f64> {
 
     let paths = read_dir("eigen_training").unwrap();
 
@@ -123,31 +166,31 @@ fn create_column_vecs_na() -> DMatrix<f32> {
         }
     }
 
-    let row_slice: &[f32] = &col_vec;
+    let row_slice: &[f64] = &col_vec;
     DMatrix::from_row_slice(im_size, num_ims, row_slice)
 }
 
-fn calculate_mean_vec_na(inp: &DMatrix<f32>) -> DVector<f32> {
+fn calculate_mean_vec_na(inp: &DMatrix<f64>) -> DVector<f64> {
 
     let mut mean_vec = Vec::with_capacity(inp.nrows());
-    let num_images = inp.len() as f32;
+    let num_images = inp.len() as f64;
 
     for i in 0..inp.nrows() {
-        let mut running_sum = 0f32;
+        let mut running_sum = 0f64;
         for j in 0..inp.ncols() {
             running_sum += inp[(i,j)];
         }
 
-        let mean_val: f32 = running_sum / num_images;
+        let mean_val: f64 = running_sum / num_images;
         mean_vec.push(mean_val.ceil());
     }
 
-    let row_slice: &[f32] = &mean_vec;
+    let row_slice: &[f64] = &mean_vec;
     DVector::from_row_slice(mean_vec.len(), row_slice)
 
 }
 
-fn reduce_matrix_na(inp_mat: &mut DMatrix<f32>, mean_vec: DVector<f32>) {
+fn reduce_matrix_na(inp_mat: &mut DMatrix<f64>, mean_vec: DVector<f64>) {
     for i in 0..inp_mat.ncols() {
         let mut this_col = inp_mat.column_mut(i);
         this_col -= &mean_vec;
@@ -178,7 +221,7 @@ fn reduce_matrix_na(inp_mat: &mut DMatrix<f32>, mean_vec: DVector<f32>) {
 
 
 
-pub fn create_column_vecs() -> Vec<Vec<f32>> {
+pub fn create_column_vecs() -> Vec<Vec<f64>> {
 
     let paths = read_dir("eigen_training").unwrap();
 
@@ -191,18 +234,18 @@ pub fn create_column_vecs() -> Vec<Vec<f32>> {
     col_vec
 }
 
-pub fn calculate_mean_vec(inp: Vec<Vec<f32>>) -> Vec<f32> {
+pub fn calculate_mean_vec(inp: Vec<Vec<f64>>) -> Vec<f64> {
 
     let mut mean_vec = Vec::with_capacity(inp[0].len());
-    let num_images = inp.len() as f32;
+    let num_images = inp.len() as f64;
 
     for i in 0..inp[0].len() {
-        let mut running_sum = 0f32;
+        let mut running_sum = 0f64;
         for j in 0..inp.len() {
-            running_sum += inp[j][i] as f32;
+            running_sum += inp[j][i] as f64;
         }
 
-        let mean_val: f32 = running_sum / num_images;
+        let mean_val: f64 = running_sum / num_images;
         mean_vec.push(mean_val.ceil());
     }
 
@@ -210,7 +253,7 @@ pub fn calculate_mean_vec(inp: Vec<Vec<f32>>) -> Vec<f32> {
 }
 
 
-pub fn reduce_matrix(inp_vec: &mut Vec<Vec<f32>>, mean_vec: Vec<f32>) {
+pub fn reduce_matrix(inp_vec: &mut Vec<Vec<f64>>, mean_vec: Vec<f64>) {
     for i in 0..inp_vec[0].len() {
         for j in 0..inp_vec.len() {
             inp_vec[j][i] -= mean_vec[i];
