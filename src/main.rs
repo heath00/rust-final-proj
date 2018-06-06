8d28975a898919f34b329fd8111c8c5afb9f0b44
heath00 <HeathReineke2018@u.northwestern.edu>
2018-06-06 12:40:23 -0500

    merge fixing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate rust_im;
 use rust_im::sobel::{sobel_detector};
 use rust_im::imgEncrypt::{encrypt,decrypt,encrypt_many,decrypt_many};
-use rust_im::traffic_detection::{count_example};
+use rust_im::traffic_detection::{count_example, class_demo};
 
 fn main() {
-    count_example("testing_car_stuff\\final_test_angle_2\\1.png", "testing_car_stuff\\final_test_angle_2\\");
-
-    //sobel_detector("in_images\\test2.jpg",
-    //              "out_images",
-    //             40);
+//    count_example("testing_car_stuff\\final_test_angle_2\\1.png", "testing_car_stuff\\final_test_angle_2\\");
+    class_demo();
+    sobel_detector("in_images\\test.jpg",
+                  "out_images",
+                 40);
     let vin = vec![60, 2];
     let vout = vec![2, 60];
     encrypt_many("in_images\\test.jpg",
